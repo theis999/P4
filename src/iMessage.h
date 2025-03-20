61295521f492dfc0f75447aece88412792a510c0
Morten <60173089+MortenSOlesen@users.noreply.github.com>
2025-03-20 10:37:45 +0100

    something works, it's like magic

--- src/iMessage.h
+++ src/iMessage.h
@@ -3,23 +3,31 @@
 #include <openssl/sha.h>
 #include "STDimport.h"
 
+using std::array;
 /// <summary>
 /// A single message in a chat channel
 /// </summary>
 class iMessage
 {
 public:
+	struct shash : public array<std::byte, 32>
+	{
+
+	};
+
 	iMessage(time_t timestamp, int member_id, string text);
-	iMessage(time_t timestamp, int member_id, string text, std::array<std::byte, 32> hash);
-	iMessage(time_t timestamp, int member_id, string text, std::array<std::byte, 32> hash, std::array<std::byte, 32> chainHash);
+	iMessage(time_t timestamp, int member_id, string text, shash hash);
+	iMessage(time_t timestamp, int member_id, string text, shash hash, shash chainHash);
 
 
 	time_t timestamp; // When the message was sent
 	int member_id; // Who sent the message
 	string text; // What was sent
 
-	std::array<std::byte, 32> hash;
-	std::array<std::byte, 32> chainHash; //missing implementation
+	
+
+	shash hash;
+	shash chainHash; //missing implementation
 
 	bool hasHash();
 	void computeHash();
